de15ed23722bcc5a6877f4f513bf74d80aa1616f
Zhao <zhaoshangming1997@gmail.com>
2018-01-15 08:34:21 +0800

    modified pre procession

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ papaer available at: https://arxiv.org/abs/1502.06922
 
 data: https://www.kaggle.com/c/quora-question-pairs/data, only use train.csv.
 
-I generated training set by randomly sampling 10,000 queries from train.csv, for each query I randomly sampled 1 similar question and 4 negative samples. For test I randomly sampled 1250 pairs of similar questions and 1250 pairs of dissimilar questions from the rest of train.csv. The validate set is generated in similar way
-
 word vector: glove.6B(300d)
 
 rename train.csv to all.csv, rename the word vector file to vector.txt, and put them under data dir
@@ -15,3 +13,5 @@ then run pre_procession.py to generate queries.txt, docs.txt, test_queries.txt, 
 pre_procession is written under py3 because I'm too lazy that I only get nltk installed under my py3 environment, I don't know if it works under py2. the other part of the program is written in py2
 
 in test we use log loss according to this page: https://www.kaggle.com/wiki/LogLoss
+
+loss decreased on train set but keep increasing on validate set, haven't figure out why. Maybe the data set is too small
--- pre_procession.py
+++ pre_procession.py
@@ -8,67 +8,91 @@ nltk.download('punkt')
 data_dir = './data'
 
 
-def create_dataset():
-    similar = {}
-    dissimilar = {}
-    data = []
+def analyze_dataset():
+    question_sets = []  # list of sets
+    dissimilar_question_sets = []
+    recorded_questions = {}  # {'index': index, 'set_index': set_index}
+    sentences = {}
     with open(os.path.join(data_dir, 'all.csv'), 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            value = {'index': [int(row[2])], 'query': row[3], 'sentence': row[4]}
-            if int(row[5]) == 1:  # similar
-                if int(row[1]) not in similar.keys():
-                    similar[int(row[1])] = value
+            idx1 = int(row[1])
+            idx2 = int(row[2])
+            is_similar = int(row[5])
+            if idx1 in recorded_questions.keys():
+                temp = recorded_questions[idx1]
+                if is_similar:
+                    if idx2 not in question_sets[temp]:
+                        question_sets[temp].add(idx2)
+                else:
+                    if idx2 not in dissimilar_question_sets[temp]:
+                        dissimilar_question_sets[temp].add(idx2)
+            elif idx2 in recorded_questions.keys():
+                temp = recorded_questions[idx2]
+                if is_similar:
+                    if idx1 not in question_sets[temp]:
+                        question_sets[temp].add(idx1)
+                else:
+                    if idx1 not in dissimilar_question_sets[temp]:
+                        dissimilar_question_sets[temp].add(idx1)
             else:
-                if int(row[1]) not in dissimilar.keys():
-                    dissimilar[int(row[1])] = value
-            data.append(row)
+                temp = len(question_sets)
+                recorded_questions[idx1] = temp
+                recorded_questions[idx2] = temp
+                question_sets.append(set([idx1]))
+                dissimilar_question_sets.append(set())
+                if is_similar:
+                    question_sets[temp].add(idx2)
+                else:
+                    dissimilar_question_sets[temp].add(idx2)
+                    question_sets.append(set([idx2]))
+                    dissimilar_question_sets.append(set([idx1]))
+            sentences[idx1] = row[3]
+            sentences[idx2] = row[4]
     train_queries = []
     train_docs = []
-    train_indexes = set()
     validate_queries = []
     validate_docs = []
     validate_ground_truths = []
     test_queries = []
     test_docs = []
     test_ground_truths = []
-    for key, value in dissimilar.items():
-        if key not in similar.keys():
-            continue
-        similar_value = similar[key]
-        train_queries.append(' '.join(word_tokenize(similar_value['query'])) + '\n')
-        train_docs.append(' '.join(word_tokenize(similar_value['sentence'])) + '\n')
-        train_docs.append(' '.join(word_tokenize(value['sentence'])) + '\n')
-        train_indexes.add(key)
-    train_queries_num = len(train_queries)
-    validate_queries_num = train_queries_num // 4
-    test_queries_num = train_queries_num // 4
-    validate_similar_count = 0
-    test_similar_count = 0
-    for line in data:
-        if (len(validate_queries) == validate_queries_num) and (len(test_queries) == test_queries_num):
-            break
-        if (int(line[1]) not in train_indexes) and (int(line[2]) not in train_indexes):
-            if int(line[5]) == 1:
-                if validate_similar_count < validate_queries_num // 2:
-                    validate_queries.append(' '.join(word_tokenize(line[3])) + '\n')
-                    validate_docs.append(' '.join(word_tokenize(line[4])) + '\n')
-                    validate_ground_truths.append(line[5] + '\n')
-                    validate_similar_count += 1
-                elif test_similar_count < test_queries_num // 2:
-                    test_queries.append(' '.join(word_tokenize(line[3])) + '\n')
-                    test_docs.append(' '.join(word_tokenize(line[4])) + '\n')
-                    test_ground_truths.append(line[5] + '\n')
-                    test_similar_count += 1
-            else:
-                if len(validate_queries) - validate_similar_count < validate_queries_num // 2:
-                    validate_queries.append(' '.join(word_tokenize(line[3])) + '\n')
-                    validate_docs.append(' '.join(word_tokenize(line[4])) + '\n')
-                    validate_ground_truths.append(line[5] + '\n')
-                elif len(test_queries) - test_similar_count < test_queries_num // 2:
-                    test_queries.append(' '.join(word_tokenize(line[3])) + '\n')
-                    test_docs.append(' '.join(word_tokenize(line[4])) + '\n')
-                    test_ground_truths.append(line[5] + '\n')
+    validate_set_size = 2000
+
+    for i, question_set in enumerate(question_sets):
+        dissimilar_question_set = dissimilar_question_sets[i]
+        if (len(question_set) >= 2 and len(dissimilar_question_set) >= 1) and (len(question_set) >= 3 or len(dissimilar_question_set) >= 2):
+            question_set = list(question_set)
+            dissimilar_question_set = list(dissimilar_question_set)
+            train_queries.append(' '.join(word_tokenize(sentences[question_set[0]])) + '\n')
+            train_docs.append(' '.join(word_tokenize(sentences[question_set[1]])) + '\n')
+            train_docs.append(' '.join(word_tokenize(sentences[dissimilar_question_set[0]])) + '\n')
+            # debug
+            # test_queries.append(' '.join(word_tokenize(sentences[question_set[0]])) + '\n')
+            # test_docs.append(' '.join(word_tokenize(sentences[question_set[1]])) + '\n')
+            # test_ground_truths.append('1\n')
+            # test_queries.append(' '.join(word_tokenize(sentences[question_set[0]])) + '\n')
+            # test_docs.append(' '.join(word_tokenize(sentences[dissimilar_question_set[0]])) + '\n')
+            # test_ground_truths.append('0\n')
+            if len(question_set) >= 3:
+                test_docs.append(' '.join(word_tokenize(sentences[question_set[2]])) + '\n')
+                test_queries.append(' '.join(word_tokenize(sentences[question_set[0]])) + '\n')
+                test_ground_truths.append('1\n')
+            if len(dissimilar_question_set) >= 2:
+                test_docs.append(' '.join(word_tokenize(sentences[dissimilar_question_set[1]])) + '\n')
+                test_queries.append(' '.join(word_tokenize(sentences[question_set[0]])) + '\n')
+                test_ground_truths.append('0\n')
+    test_queries = test_queries[:validate_set_size*2]
+    test_docs = test_docs[:validate_set_size*2]
+    test_ground_truths = test_ground_truths[:validate_set_size*2]
+    validate_queries = test_queries[:validate_set_size]
+    validate_docs = test_docs[:validate_set_size]
+    validate_ground_truths = test_ground_truths[:validate_set_size]
+    test_queries = test_queries[validate_set_size:]
+    test_docs = test_docs[validate_set_size:]
+    test_ground_truths = test_ground_truths[validate_set_size:]
+
+    print("train query number: %d, validate query number: %d, test query number: %d" % (len(train_queries), len(validate_queries), len(test_queries)))
 
     with open(os.path.join(data_dir, 'queries.txt'), 'w') as f:
         f.writelines(train_queries)
@@ -162,5 +186,5 @@ def create_dataset_deprecated():
         f.writelines(validate_ground_truths)
 
 if __name__ == '__main__':
-    create_dataset()
-    # analyze_dataset()
+    # create_dataset()
+    analyze_dataset()
